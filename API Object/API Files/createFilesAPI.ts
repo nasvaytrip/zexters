d5cb3cab9080e0c2e645db818e3f7251370ac904
nasvaytrip <nasvaytrip@gmail.com>
2024-06-20 17:58:12 +0300

    Switch server #2

--- API Object/API Files/createFilesAPI.ts
+++ API Object/API Files/createFilesAPI.ts
@@ -7,7 +7,7 @@ constructor (page) {
     super (page);}
 
 async createDocument(name) {
-    const response = await this.page.request.post('https://gb-auto-r-srv2.demo.zextras.io/services/docs/files/create', {
+    const response = await this.page.request.post('https://gb-auto-u-srv2.demo.zextras.io/services/docs/files/create', {
     data: {filename: name, type: "LIBRE_DOCUMENT", destinationFolderId: "LOCAL_ROOT"}
     });
     const body = await this.GetResponceBody(response);
@@ -17,7 +17,7 @@ async createDocument(name) {
 }
 
 async createGroup(name) {
-    const response = await this.page.request.post('https://gb-auto-r-srv2.demo.zextras.io/zx/team/v22/createGroup', {
+    const response = await this.page.request.post('https://gb-auto-u-srv2.demo.zextras.io/zx/team/v22/createGroup', {
     data: {invited_user_ids:["ea8f7cc4-54e7-407c-87ae-ba22764117c7","75bbeae5-5120-4d77-96d1-b1451d23f9c2"],topic: name}
 
     });
@@ -26,14 +26,13 @@ async createGroup(name) {
 }
 
 async kickFromGroup(konvid) {
-    const response = await this.page.request.post('https://gb-auto-r-srv2.demo.zextras.io/zx/team/v22/kickFromConversation', {
+    const response = await this.page.request.post('https://gb-auto-u-srv2.demo.zextras.io/zx/team/v22/kickFromConversation', {
     data: {conversation_id: konvid ,kicked_user_ids:["75bbeae5-5120-4d77-96d1-b1451d23f9c2"]}
     });
     await this.GetResponceBody(response);
 }
-
 async getIdFromAllGroups() {
-    const response = await this.page.request.post('https://gb-auto-r-srv2.demo.zextras.io/zx/team/v22/getConversations', {
+    const response = await this.page.request.post('https://gb-auto-u-srv2.demo.zextras.io/zx/team/v22/getConversations', {
         data: {}
         });
        const body = await this.GetResponceBody(response);
@@ -43,31 +42,31 @@ async getIdFromAllGroups() {
 }
 
 async kickFromGroupTwo() {
-    const response = await this.page.request.post('https://gb-auto-r-srv2.demo.zextras.io/zx/team/v22/kickFromConversation', {
+    const response = await this.page.request.post('https://gb-auto-u-srv2.demo.zextras.io/zx/team/v22/kickFromConversation', {
     data: {conversation_id:"ab5b8f6c-31f4-4f79-8467-358a67e5fd9c@demo.zextras.io",kicked_user_ids:["ea8f7cc4-54e7-407c-87ae-ba22764117c7"]}
     });
     await this.GetResponceBody(response);
 }
 
 async leaveFromGroup() {
-    const response = await this.page.request.post('https://gb-auto-r-srv2.demo.zextras.io/zx/team/v22/leaveConversation', {
+    const response = await this.page.request.post('https://gb-auto-u-srv2.demo.zextras.io/zx/team/v22/leaveConversation', {
     data: {conversation_id:"ab5b8f6c-31f4-4f79-8467-358a67e5fd9c@demo.zextras.io"}
     });
     await this.GetResponceBody(response);
 }
 
 async deleteGroup(konvid) {
-    const response = await this.page.request.post('https://gb-auto-r-srv2.demo.zextras.io/zx/team/v22/kickFromConversation', {
+    const response = await this.page.request.post('https://gb-auto-u-srv2.demo.zextras.io/zx/team/v22/kickFromConversation', {
         data: {conversation_id: konvid ,kicked_user_ids:["75bbeae5-5120-4d77-96d1-b1451d23f9c2"]}
         });
         await this.GetResponceBody(response);
     
-    const responsea = await this.page.request.post('https://gb-auto-r-srv2.demo.zextras.io/zx/team/v22/kickFromConversation', {
+    const responsea = await this.page.request.post('https://gb-auto-u-srv2.demo.zextras.io/zx/team/v22/kickFromConversation', {
         data: {conversation_id: konvid ,kicked_user_ids:["ea8f7cc4-54e7-407c-87ae-ba22764117c7"]}
         });
         await this.GetResponceBody(responsea);
 
-         const responseb = await this.page.request.post('https://gb-auto-r-srv2.demo.zextras.io/zx/team/v22/leaveConversation', {
+         const responseb = await this.page.request.post('https://gb-auto-u-srv2.demo.zextras.io/zx/team/v22/leaveConversation', {
             data: {conversation_id: konvid}
             });
             await this.GetResponceBody(responseb);
--- API Object/API Mails/deleteMailAPI.ts
+++ API Object/API Mails/deleteMailAPI.ts
@@ -5,7 +5,7 @@ export class DeleteMailApi extends baseAPI {
         super (page);}
 
     async deleteMail() {
-        const response = await this.page.request.post('https://gb-auto-r-srv2.demo.zextras.io/service/soap/ConvActionRequest', {
+        const response = await this.page.request.post('https://gb-auto-u-srv2.demo.zextras.io/service/soap/ConvActionRequest', {
             data: {"Body":{"MsgActionRequest":{"_jsns":"urn:zimbraMail","action":{"id":"320","op":"delete"}}},"Header":{"context":{"_jsns":"urn:zimbra","session":{"id":"126769","_content":"126769"},"account":{"by":"name","_content":"test174@demo.zextras.io"},"userAgent":{"name":"CarbonioWebClient - Chrome 88.0.4324.188 (Chromecast)","version":"24.5.0_ZEXTRAS_202405 carbonio 20240517-1011 FOSS"}}}}
             });
             await this.GetResponceBody(response);
