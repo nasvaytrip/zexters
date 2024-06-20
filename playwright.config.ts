001bff58364930d700e70bee78050a8c14ccab75
nasvaytrip <nasvaytrip@gmail.com>
2024-06-20 22:12:25 +0300

    Timeout +30000

--- playwright.config.ts
+++ playwright.config.ts
@@ -24,6 +24,7 @@ export default defineConfig({
     ['list'],
     ['allure-playwright']
   ],
+  timeout: 60000,
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
     // baseURL: 'http://127.0.0.1:3000',
